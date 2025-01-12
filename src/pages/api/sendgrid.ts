eb3b7d940cb0db4315d6b44cad88b19a29febab0
Agustin <aencaje@gmail.com>
2025-01-12 12:27:41 -0300

    Fix more lint

--- src/pages/api/sendgrid.ts
+++ src/pages/api/sendgrid.ts
@@ -1,33 +1,48 @@
-import sendgrid from "@sendgrid/mail";
+import sendgrid from '@sendgrid/mail';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+const apiKey = process.env.SENDGRID_API_KEY;
 
-async function sendEmail(req, res) {
-	try {
-		if (!req.body.message || req.body.message === "") {
-			throw new Error("Message empty");
-		}
-		await sendgrid.send({
-			to: process.env.SENDGRID_TO_EMAIL,
-			from: process.env.SENDGRID_FROM_EMAIL,
-			subject: "[daily.place] Contact",
-			content: [
-				{
-					type: "text/html",
-					value: `<div>
-                  <p>Name: ${req.body.name}</p>
-                  <p>Email: ${req.body.email}</p>
-                  <p>Message: ${req.body.message}</p>
-                  </div>`,
-				},
-			],
-		});
-	} catch (error) {
-		console.log(error.message);
-		return res.status(error.statusCode || 500).json({ error: error.message });
-	}
+if (!apiKey) {
+    throw new Error('SENDGRID_API_KEY is not defined');
+}
 
-	return res.status(200).json({ error: "" });
+sendgrid.setApiKey(apiKey);
+
+async function sendEmail(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        if (!req.body.message || req.body.message === '') {
+            throw new Error('Message empty');
+        }
+        const toEmail = process.env.SENDGRID_TO_EMAIL;
+        const fromEmail = process.env.SENDGRID_FROM_EMAIL;
+
+        if (!toEmail || !fromEmail) {
+            throw new Error('SENDGRID_TO_EMAIL or SENDGRID_FROM_EMAIL is not defined');
+        }
+
+        await sendgrid.send({
+            to: toEmail,
+            from: fromEmail,
+            subject: '[daily.place] Contact',
+            content: [
+                {
+                    type: 'text/html',
+                    value: `<div>
+					<p>Name: ${req.body.name}</p>
+					<p>Email: ${req.body.email}</p>
+					<p>Message: ${req.body.message}</p>
+					</div>`
+                }
+            ]
+        });
+    } catch (error) {
+        const errorMessage = (error as Error).message;
+        console.log(errorMessage);
+        return res.status((error as any).statusCode || 500).json({ error: errorMessage });
+    }
+
+    return res.status(200).json({ error: '' });
 }
 
 export default sendEmail;
